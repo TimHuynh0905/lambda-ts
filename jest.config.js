3be878ae00c617f5d16ce1d8c2bbd20d6616d395
TimHuynh0905 <timhuynh0905@gmail.com>
2022-11-05 14:46:55 -0500

    test: created jest config file for unit testing

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  rootDir: "./",
+  preset: "ts-jest",
+  verbose: true,
+
+  testEnvironment: "jest-environment-node",
+  testMatch: ["<rootDir>/tests/**/?(*.)+(spec|test).ts?"],
+
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts?"],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  coverageReporters: ["json", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
